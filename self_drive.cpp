e99125a9e8e5f28b2687081eb5229b01ccb98a8f
leehyeonjin <zzxx9315@naver.com>
2023-12-12 10:10:05 +0900

    Follow the right side wall of the robot

--- self_drive.cpp
+++ self_drive.cpp
@@ -22,8 +22,7 @@ public:
     auto lidar_qos_profile = rclcpp::QoS(rclcpp::KeepLast(1));
     lidar_qos_profile.reliability(rclcpp::ReliabilityPolicy::BestEffort);
     auto callback = std::bind(&SelfDrive::subscribe_scan, this, std::placeholders::_1);
-    scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", lidar_qos_profile,
-                                                                       callback);
+    scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", lidar_qos_profile, callback);
     auto vel_qos_profile = rclcpp::QoS(rclcpp::KeepLast(1));
     pose_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", vel_qos_profile);
   }
@@ -31,26 +30,28 @@ public:
   void subscribe_scan(const sensor_msgs::msg::LaserScan::SharedPtr scan)
   {
     geometry_msgs::msg::Twist vel;
-   
-    vel.linear.x = 0.15;
-    vel.angular.z = 0.0;
-    if( scan->ranges[0] < 0.25 || scan->ranges[340] < 0.25)
+
+    if (scan->ranges[0] < 0.25 || scan->ranges[340] < 0.25)
     {
-      vel.linear.x = 0.0;
-      vel.angular.z = 2.0;
+      vel.linear.x = 0.;
+      vel.angular.z = 0.75;
     }
-    else if( scan->ranges[270] < 0.25 && !(scan->ranges[310] < 0.25))
+    else if (((scan->ranges[270] < 0.25) || (scan->ranges[250] < 0.25) || (scan->ranges[230] < 0.25) || (scan->ranges[200] < 0.25 || (scan->ranges[190] < 0.25))) && !(scan->ranges[320] < 0.25))
     {
-      vel.linear.x = 0.0;
-      vel.angular.z = -13.0;
+      vel.linear.x = 0.1;
+      vel.angular.z = -0.3;
     }
-    RCLCPP_INFO(rclcpp::get_logger("self_drive"),
-                "step=%d, range=%1.2f, linear=%1.2f, angular=%1.2f", step_, scan->ranges[0],
-                vel.linear.x, vel.angular.z);
+    else
+    {
+      vel.linear.x = 0.15;
+      vel.angular.z = 0.;
+    }
+
+    RCLCPP_INFO(rclcpp::get_logger("self_drive"), "step=%d, range=%1.2f, linear=%1.2f, angular=%1.2f", step_,
+                scan->ranges[0], vel.linear.x, vel.angular.z);
     pose_pub_->publish(vel);
     step_++;
   }
-  
 };
 
 int main(int argc, char* argv[])
