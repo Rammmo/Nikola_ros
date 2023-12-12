cf669a5f9ebcf763d87c232555d7f50893812fa5
leehyeonjin <zzxx9315@naver.com>
2023-12-12 12:50:03 +0900

    again

--- self_drive.cpp
+++ self_drive.cpp
@@ -30,24 +30,14 @@ public:
   void subscribe_scan(const sensor_msgs::msg::LaserScan::SharedPtr scan)
   {
     geometry_msgs::msg::Twist vel;
-
-    if (scan->ranges[0] < 0.25 || scan->ranges[340] < 0.25)
+ if (scan->ranges[0] < 0.25 || scan->ranges[340] < 0.25)
     {
       vel.linear.x = 0.;
       vel.angular.z = 0.75;
     }
-    else if (((scan->ranges[270] < 0.25) || (scan->ranges[250] < 0.25) || (scan->ranges[230] < 0.25) ||
-              (scan->ranges[200] < 0.25 || (scan->ranges[190] < 0.25))) &&
-             !(scan->ranges[320] < 0.25))
-    {
-      vel.linear.x = 0.1;
-      vel.angular.z = -0.3;
-    }
-    else
-    {
       vel.linear.x = 0.15;
       vel.angular.z = 0.;
-    }
+
 
     RCLCPP_INFO(rclcpp::get_logger("self_drive"), "step=%d, range=%1.2f, linear=%1.2f, angular=%1.2f", step_,
                 scan->ranges[0], vel.linear.x, vel.angular.z);
